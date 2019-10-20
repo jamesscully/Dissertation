a361eaf110d262671ea071a8cfc320fede2f3be2
James Scully <james.will.scully@gmail.com>
2019-10-20 19:51:41 +0100

    Added pullCard method to get an available card from deck, private removeCard to update local members of removal

--- texas/src/card/Deck.java
+++ texas/src/card/Deck.java
@@ -1,15 +1,17 @@
 package card;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class  Deck {
 
     private static Deck instance;
+    private int cards_removed = 0;
 
     /**
      * Holds all cards available
      */
-    ArrayList<Card> deck = new ArrayList<>();
+    private ArrayList<Card> deck = new ArrayList<>();
 
     public Deck() { }
 
@@ -24,22 +26,65 @@ public class  Deck {
      * Populate {@link Deck#deck} with all needed cards.
      */
     public void initialize() {
+        resetDeck();
+    }
+
+    public void resetDeck() {
+        cards_removed = 0;
         for(Suit s : Suit.values()) {
             for(Value v : Value.values()) {
                 deck.add(new Card(s, v));
             }
         }
+    }
 
+    /**
+     * Removes card from the deck.
+     * @param c Card to remove
+     * @return True if remove was successful
+     */
+    private boolean removeCard(Card c) {
+        int value = c.getValue();
+        int sID   = c.suit.id;
+
+        int position = ( (value * sID) + (13 % value) ) - cards_removed;
+
+        try
+        {
+            deck.remove(position);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.err.println("Index " + position + " was out of bounds in deck.");
+            return false;
+        }
+
+        cards_removed++;
+        return true;
+    }
+
+    /**
+     * Pulls a card from the deck for use in a Hand
+     * @return Card retrieved from deck
+     */
+    public Card pullCard() {
+        Random random = new Random();
+
+        Card pull = Card.getRandomCard();
+
+        // if removing the card returns false, try again
+        while(!removeCard(pull))
+            pull = Card.getRandomCard();
+
+        return pull;
+    }
+
+    /**
+     * Prints the deck as it is currently.
+     */
+    public void printDeck() {
         int i = 1;
         for(Card c : deck) {
-
-            if(i % 13 == 1)
-                System.out.println();
-
-            System.out.println(i + " : " + c);
-
-
-
+            System.out.printf("%2d: %17s\n", i, c);
             i++;
         }
     }
