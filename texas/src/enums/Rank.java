359c7e1eeacfa2a187ce47125e444ee595bde61b
James Scully <james.will.scully@gmail.com>
2019-11-26 20:18:04 +0000

    Rank now shows toString

--- texas/src/enums/Rank.java
+++ texas/src/enums/Rank.java
@@ -12,6 +12,22 @@ public enum Rank {
         STRAIGHT_FLUSH(9),
         ROYAL_FLUSH(10);
 
+        @Override
+        public String toString() {
+                switch (value) {
+                        case 1: return "High Card"; break;
+                        case 2: return "One Pair"; break;
+                        case 3: return "Two Pair"; break;
+                        case 4: return "Three of a Kind"; break;
+                        case 5: return "Straight"; break;
+                        case 6: return "Flush"; break;
+                        case 7: return "Full House"; break;
+                        case 8: return "Four of a Kind"; break;
+                        case 9: return "Straight Flsuh"; break;
+                        case 10: return "Royal Flush"; break;
+                }
+        }
+
         private final int value;
 
         Rank(int val) {
