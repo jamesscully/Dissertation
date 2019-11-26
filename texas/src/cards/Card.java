0290280092f83d2e264d0c59d1c0cc29ba955e2f
James Scully <james.will.scully@gmail.com>
2019-11-26 18:17:37 +0000

    Move debug card gen to Card file

--- texas/src/cards/Card.java
+++ texas/src/cards/Card.java
@@ -66,9 +66,55 @@ public class Card implements Comparable<Card> {
         return new Card(s, v);
     }
 
+
+
     @Override
     public int compareTo(Card card) {
 
         return getValueInteger().compareTo(card.getValueInteger());
     }
+
+    public static Card strToCard(String s) {
+
+        s = s.toUpperCase();
+
+        Face face = null;
+        Suit suit = null;
+
+        switch (s.charAt(0)) {
+            case 'A': face = Face.ACE;   break;
+            case 'K': face = Face.KING;  break;
+            case 'Q': face = Face.QUEEN; break;
+            case 'J': face = Face.JACK;  break;
+            case '0': face = Face.TEN;   break;
+            case '9': face = Face.NINE;  break;
+            case '8': face = Face.EIGHT; break;
+            case '7': face = Face.SEVEN; break;
+            case '6': face = Face.SIX;   break;
+            case '5': face = Face.FIVE;  break;
+            case '4': face = Face.FOUR;  break;
+            case '3': face = Face.THREE; break;
+            case '2': face = Face.TWO;   break;
+
+            default:
+                System.err.println(s.charAt(0) + " is not a valid face!");
+                System.exit(1);
+                break;
+        }
+
+        switch (s.charAt(1)) {
+            case 'D': suit = Suit.DIAMONDS; break;
+            case 'S': suit = Suit.SPADES;   break;
+            case 'C': suit = Suit.CLUBS;    break;
+            case 'H': suit = Suit.HEARTS;   break;
+
+            default:
+                System.err.println(s.charAt(1) + " is not a valid suit!");
+                System.exit(1);
+                break;
+        }
+
+        return new Card(suit, face);
+    }
+
 }
